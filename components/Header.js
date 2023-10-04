c539321ebdc60678a4365e5462b65a136d87be2d
Yousef <yousefsameh24@gmail.com>
2023-10-04 11:24:09 +0300

    update color

--- components/Header.js
+++ components/Header.js
@@ -27,14 +27,14 @@ export default function Header(){
                     </div>
                     <ul className={`flex-1 justify-between mt-12 md:flex md:mt-0 hidden`}>
                         <li className="order-2 pb-5 md:pb-0">
-                            <a href="#" className="py-3 px-6 rounded-md shadow-md text-white text-center bg-cyan-500 focus:shadow-none block md:inline">
+                            <a href="#" className="py-3 px-6 rounded-md shadow-md text-white text-center bg-[#7bc7c1] focus:shadow-none block md:inline">
                                 تسجيل الدخول
                             </a>
                         </li>
                         <div className="order-1 flex-1 justify-center flex gapItems items-center space-y-5 md:flex md:space-x-6 md:space-x-reverse md:space-y-0">
                             {
                                 navigation.map((item, idx) => (
-                                    <li className="text-gray-500 hover:text-cyan-600" key={idx}>
+                                    <li className="text-gray-500 hover:text-[#7bc7c1]" key={idx}>
                                         <a href={item.path}>{item.title}</a>
                                     </li>
                                 ))
